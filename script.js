95a644ff227b36a5bb625839e7b0bf22637d4435
Marco Bacci <marcobacci94@gmail.com>
2025-07-29 14:40:23 +0200

    step 2

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// primo step
 const getNames = require("./modules/names");
 
 const person = getNames("Marco", "Bacci");
 
 console.log(person);
+
+// secondo step
+const getHobbies = require('./modules/hobbies')
+
+const hobbies = getHobbies('calcio', 'nuoto', 'tennis') 
+
+console.log(hobbies)
